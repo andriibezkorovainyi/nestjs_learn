4725161aa56cf7a6b4332ce9ab101b7120e5e14e
Andrii Bezkorovainyi <crm29865@dz.com>
2023-06-28 16:36:41 +0300

    created controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response, Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('app')
+  getHello(@Res() res: Response, @Req() req: Request): string {
     return this.appService.getHello();
   }
 }
